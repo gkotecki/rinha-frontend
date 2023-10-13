8a35eaadc9ae45d2178cca593644f0c6455a77c2
gkotecki <gabrielkotecki@gmail.com>
2023-10-13 15:33:34 -0300

    ADD: tailwind, prettier, dotfiles

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  singleAttributePerLine: false,
+}
