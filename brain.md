2c2d241867454010d90feffec90581454bda4159
gkotecki <gabrielkotecki@gmail.com>
2023-10-23 17:37:20 -0300

    ADD: external code refs

---
+++ brain.md
@@ -0,0 +1,7 @@
+## TODO
+- Parse directly if size is small than threshold
+- Add parameters to control the size of buffer, threshold, etc.
+- Study web workers implementations
+
+## References
+- https://github.com/dominictarr/JSONStream
---
+++ src/uint8-to-str.ts
@@ -0,0 +1,54 @@
+/**
+ * Transformer class taken from:
+ * https://github.com/mdn/dom-examples/tree/main/streams
+ */
+
+/**
+ * This transformer takes binary Uint8Array chunks from a `fetch`
+ * and translates them to chunks of strings.
+ *
+ * @implements {TransformStreamTransformer}
+ */
+export class Uint8ArrayToStringsTransformer {
+  private decoder = new TextDecoder()
+  private lastString = ''
+
+  /**
+   * Receives the next Uint8Array chunk from `fetch` and transforms it.
+   *
+   * @param {Uint8Array} chunk The next binary data chunk.
+   * @param {TransformStreamDefaultController} controller The controller to enqueue the transformed chunks to.
+   */
+  transform(chunk: Uint8Array, controller: TransformStreamDefaultController) {
+    // console.log(`Received chunk %o with %o bytes.`, chunk, chunk.byteLength)
+
+    // Decode the current chunk to string and prepend the last string
+    const string = `${this.lastString}${this.decoder.decode(chunk)}`
+
+    // console.log(string)
+
+    // Extract lines from chunk
+    const lines = string.split(/\r\n|[\r\n]/g)
+
+    // Save last line, as it might be incomplete
+    this.lastString = lines.pop() || ''
+
+    // Enqueue each line in the next chunk
+    for (const line of lines) {
+      controller.enqueue(line)
+    }
+  }
+
+  /**
+   * Is called when `fetch` has finished writing to this transform stream.
+   *
+   * @param {TransformStreamDefaultController} controller The controller to enqueue the transformed chunks to.
+   */
+  flush(controller: TransformStreamDefaultController) {
+    console.log('>> caled flush')
+    // Is there still a line left? Enqueue it
+    if (this.lastString) {
+      controller.enqueue(this.lastString)
+    }
+  }
+}
